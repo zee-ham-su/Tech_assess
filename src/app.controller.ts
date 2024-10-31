f0fca636db90ba2b7131113903d536146cf1e81d
zee-ham-su <hsufiian@yahoo.com>
2024-10-31 17:12:11 +0000

    Fix root route to return welcome message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('api/v1/welcome')
   getWelcomeMessage(): string {
     return this.appService.getWelcomeMessage();
   }
